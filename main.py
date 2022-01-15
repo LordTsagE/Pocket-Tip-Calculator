ec9fe1bd5c7728b48c1c9ae135aa6dfb447144c4
Gabriel Nwokolo <92697325+LordTsagE@users.noreply.github.com>
2022-01-15 17:14:20 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+#If the bill was $150.00, split between 5 people, with 12% tip. 
+
+#Each person should pay (150.00 / 5) * 1.12 = 33.6
+#Format the result to 2 decimal places = 33.60
+
+#Tip: There are 2 ways to round a number. You might have to do some Googling to solve this.💪
+
+#Write your code below this line 👇
+print("Welcome to the tip calculator.")
+total_bill = input("What was the total bill?")
+itb = float(total_bill)
+percentage_tip = input("What percentage tip would you like to give?")
+ipt = float(percentage_tip)
+number_ppl= input("How many people to split the bill?")
+inp = float(number_ppl)
+p = (itb * ipt/100)
+total_to_pay = (itb + p)
+amount_per_person = total_to_pay/inp
+app = round(amount_per_person, 2)
+print(f"Each person should pay ${app}")
